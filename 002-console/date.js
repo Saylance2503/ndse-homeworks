7a8a4f0a6687424bc859d866a9d1345006d36a93
Yuriy Starostin <yurik_05@inbox.ru>
2023-12-11 22:46:10 +0400

    feat: solution

--- 002-console/date.js
+++ 002-console/date.js
@@ -1,44 +1,85 @@
-const { argv } = require("yargs");
-const { _: commands, d, date, m, month, y, year } = argv;
-const newDate = new Date();
-const command = commands.toString();
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
 
-let params, period;
+const argv = yargs(hideBin(process.argv))
+    .command('current', 'Получить текущую дату и время в формате ISO', {
+        year: {
+            alias: 'y',
+            describe: 'Получить текущий год',
+            type: 'boolean',
+        },
+        month: {
+            alias: 'm',
+            describe: 'Получить текущий месяц',
+            type: 'boolean',
+        },
+        date: {
+            alias: 'd',
+            describe: 'Получить текущую дату',
+            type: 'boolean',
+        },
+    })
+    .command('add', 'Добавить время к текущей дате и времени', {
+        date: {
+            alias: 'd',
+            describe: 'Количество дней для добавления',
+            type: 'number',
+        },
+        month: {
+            alias: 'm',
+            describe: 'Количество месяцев для добавления',
+            type: 'number',
+        },
+    })
+    .command('sub', 'Вычесть время из текущей даты и времени', {
+        date: {
+            alias: 'd',
+            describe: 'Количество дней для вычитания',
+            type: 'number',
+        },
+        month: {
+            alias: 'm',
+            describe: 'Количество месяцев для вычитания',
+            type: 'number',
+        },
+    })
+    .demandCommand(1, 'Вы должны указать команду')
+    .help().argv;
 
-if (year || y) {
-    period = "year";
-    params = year || y;
-} else if (month || m) {
-    period = "month";
-    params = month || m;
-} else if (date || d) {
-    period = "date";
-    params = date || d;
-}
+const currentDate = new Date();
 
-const getResultCurrentCommand = () => {
-    const modelObj = {
-        year: () => newDate.getFullYear(),
-        month: () => newDate.getMonth(),
-        date: () => newDate.getDate(),
-    };
-    return modelObj[period]() || newDate;
+const executeCommand = () => {
+    switch (argv._[0]) {
+        case 'current':
+            if (argv.year) {
+                console.log(currentDate.getFullYear());
+            } else if (argv.month) {
+                console.log(currentDate.getMonth() + 1);
+            } else if (argv.date) {
+                console.log(currentDate.toISOString().split('T')[0]);
+            } else {
+                console.log(currentDate.toISOString());
+            }
+            break;
+        case 'add':
+            if (argv.date) {
+                currentDate.setDate(currentDate.getDate() + argv.date);
+            } else if (argv.month) {
+                currentDate.setMonth(currentDate.getMonth() + argv.month);
+            }
+            console.log(currentDate.toISOString());
+            break;
+        case 'sub':
+            if (argv.date) {
+                currentDate.setDate(currentDate.getDate() - argv.date);
+            } else if (argv.month) {
+                currentDate.setMonth(currentDate.getMonth() - argv.month);
+            }
+            console.log(currentDate.toISOString());
+            break;
+        default:
+            console.log('Неверная команда');
+    }
 };
 
-const getCommandAdd = (add) => {
-    const modelObj = {
-        year: () => newDate.getFullYear() + add,
-        month: () => newDate.getMonth() + add,
-        date: () => newDate.getDate() + add,
-    };
-    return modelObj[period]() || newDate;
-};
-
-const getCommandSub = (sub) => {
-    const modelObj = {
-        year: () => newDate.getFullYear() - sub,
-        month: () => newDate.getMonth() - sub,
-        date: () => newDate.getDate() - sub,
-    };
-    return modelObj[period]() || newDate;
-};
+executeCommand();
